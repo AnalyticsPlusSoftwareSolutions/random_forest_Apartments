6f738e9bd3e6f70124880fbbda775f51e78c738f
Alicja Gosiewska <alicjagosiewska@gmail.com>
2018-07-21 18:07:28 +0200

    Fix pred last time

--- R/pred.R
+++ R/pred.R
@@ -7,5 +7,8 @@
 #' @export
 
 pred <- function(input) {
+  input$district <- factor(input$district,
+                           levels = c("Bemowo", "Bielany", "Mokotow", "Ochota", "Praga",
+                                      "Srodmiescie" ,"Ursus" , "Ursynow" , "Wola", "Zoliborz" ))
   predict(object = modelApartmentsRF::model, newdata = input)
 }
